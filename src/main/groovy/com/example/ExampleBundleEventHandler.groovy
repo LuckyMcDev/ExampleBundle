2726a7ea01c46390a81d2236686b6824d9708f25
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-03-21 12:32:11 +0100

    Update to 26.1-pre3

--- src/main/groovy/com/example/ExampleBundleEventHandler.groovy
+++ src/main/groovy/com/example/ExampleBundleEventHandler.groovy
@@ -1,29 +1,31 @@
 package com.example
 
-import io.github.luckymcdev.foundryengine.common.registry.builder.ItemBuilder
+import io.github.luckymcdev.foundryengine.api.builder.item.ItemBuilder
 import io.github.luckymcdev.foundryengine.common.util.Freezable
 import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.Registries
 import net.minecraft.resources.Identifier
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.Items
 import net.minecraft.world.item.Rarity
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.neoforge.registries.RegisterEvent
 
 class ExampleBundleEventHandler {
-    public static Freezable<Item> EXAMPLE_ITEM = new Freezable<>()
+    public static Item EXAMPLE_ITEM;
 
     @SubscribeEvent
     static void onRegisterEvent(RegisterEvent event) {
         event.register(Registries.ITEM, helper -> {
-            def item = new ItemBuilder(ExampleBundle.id("example_item"))
-                    .component(DataComponents.RARITY, Rarity.EPIC)
+            def item = ItemBuilder.create(ExampleBundle.id("example_item"))
                     .properties( p -> {
                         p.fireResistant()
+                        p.rarity(Rarity.EPIC)
+                        p.repairable(Items.ACACIA_DOOR)
+                        p.stacksTo(10)
                     })
-
-            EXAMPLE_ITEM.setValue(item.register(helper))
-            EXAMPLE_ITEM.freeze()
+            EXAMPLE_ITEM = item.get()
+            item.register(helper)
         })
     }
 }
