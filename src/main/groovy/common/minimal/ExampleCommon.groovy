2562bee9f39d83a60ec7bff79f33eb9565c8f549
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-04-13 16:36:11 +0200

    Refactor bundle stuff to follow new API.

---
+++ src/main/groovy/common/minimal/ExampleCommon.groovy
@@ -0,0 +1,14 @@
+package common.minimal
+
+import de.luckymcdev.foundryengine.common.script.BundleEntrypoint
+
+class ExampleCommon implements BundleEntrypoint {
+
+    @Override
+    void onLoad() {
+    }
+
+    @Override
+    void onUnload() {
+    }
+}
---
+++ src/main/groovy/server/minimal/ExampleServer.groovy
@@ -0,0 +1,14 @@
+package server.minimal
+
+import de.luckymcdev.foundryengine.common.script.BundleEntrypoint
+
+class ExampleServer implements BundleEntrypoint {
+
+    @Override
+    void onLoad() {
+    }
+
+    @Override
+    void onUnload() {
+    }
+}
