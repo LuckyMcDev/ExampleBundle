c816ede803ac023c9732e0b331d3e3528ab02403
LuckyMcDev <191139832+LuckyMcDev@users.noreply.github.com>
2026-07-30 13:31:28 +0200

    Revert "Merge pull request #1 from LuckyMcDev/testbundle"
    This reverts commit 3b79a62e598c731ae11592989711a6c0523e2594, reversing
    changes made to 3ee99c41386466341a69c02533c8cfaa809de064.

--- src/main/groovy/server/minimal/ExampleServer.groovy
+++ src/main/groovy/server/minimal/ExampleServer.groovy
@@ -1,13 +1,11 @@
-package server.testbundle
-
+package server.minimal
 
 import de.luckymcdev.foundryengine.common.script.BundleEntrypoint
 
-class ServerEntrypoint implements BundleEntrypoint {
+class ExampleServer implements BundleEntrypoint {
 
     @Override
     void onLoad() {
-
     }
 
     @Override
